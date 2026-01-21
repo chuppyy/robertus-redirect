20904fbf7d85e47fd0b6993071b66e0e2230c7ed
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-21 08:17:08 +0700

    Update domain.tsx

--- domain.tsx
+++ domain.tsx
@@ -1,2 +1,3 @@
-export const domain = "https://topnewsus.feji.io";
+export const domain = "https://vtus.cafex.biz";
+//export const domain = "https://topnewsus.feji.io";
 //export const domain = "https://topnews.livextop.com";
